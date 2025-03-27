1a26f30c0624cd040ff444bf341b0464765e3fef
Maria Carolina <mariacarolina1701@outlook.com>
2025-03-27 12:54:16 -0300

    add jest

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Lint & prettify all files on commit
+  '*.{js,ts,tsx, jsx, css, scss,md,json}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
+  ]
+}
