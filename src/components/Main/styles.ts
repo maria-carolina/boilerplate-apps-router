ac0c90fc9af5d0cbcfb40ec439681d3432dfc17c
Maria Carolina <mariacarolina1701@outlook.com>
2025-04-02 19:42:14 -0300

    add controls do storybook

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -9,5 +9,23 @@ export const Wrapper = styled.main`
   justify-content: center;
   width: 100%;
   height: 100vh;
-  background-color: #f0f0f0;
+  background-color: rgb(192, 133, 133);
+`
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
 `
